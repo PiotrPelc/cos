1d437b6f85527a211f79c3e5076efbbe3d714498
pelcp <s95521@pollub.edu.pl>
2024-10-10 09:36:50 +0200

    fix

--- src/main/java/com/lg/main.java
+++ src/main/java/com/lg/main.java
@@ -2,7 +2,7 @@ package com.lg;
 
 public class main {
     public static void main(String[] args) {
-        System.out.println("Hello World2");
+        System.out.println("Hello World");
         System.out.println("Czesc2");
     }
 }
